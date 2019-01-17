0911dedf91352c82a901c5760e53a2f339900518
Tepy Thai <thaitepy@gmail.com>
2019-01-17 10:01:50 +0900

    moved some logos' link to cloudinary

--- src/images/logos.js
+++ src/images/logos.js
@@ -1,15 +1,15 @@
 export const ReactLogo =
-  'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
+  'https://res.cloudinary.com/dzitawely/image/upload/v1547686725/ueit0jrnhigbipssbpco.svg'
 export const GraphqlLogo =
   'https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg'
 export const GatsbyLogo = 'https://www.gatsbyjs.org/monogram.svg'
 export const RebassLogo =
   'https://camo.githubusercontent.com/4f6ca9438a3e45f9b409158503f3deebc86a793d/68747470733a2f2f7265626173736a732e6f72672f6c6f676f2e737667'
 export const JavaLogo =
-  'https://cdn.iconscout.com/icon/premium/png-256-thumb/java-file-format-509610.png'
+  'https://res.cloudinary.com/dzitawely/image/upload/c_scale,w_16/v1547686667/java-file-format-509610.png'
 
 export const TheMovieDBLogo =
-  'https://www.themoviedb.org/assets/1/v4/logos/312x276-primary-green-74212f6247252a023be0f02a5a45794925c3689117da9d20ffe47742a665c518.png'
+  'https://res.cloudinary.com/dzitawely/image/upload/c_scale,w_16/v1547686574/293x302-powered-by-square-green-3ee4814bb59d8260d51efdd7c124383540fc04ca27d23eaea3a8c87bfa0f388d.png'
 
 export const SquareLogo =
-  'https://res.cloudinary.com/dzitawely/image/upload/v1547514566/portfolio/square-logo.png'
+  'https://res.cloudinary.com/dzitawely/image/upload/c_scale,w_16/v1547514566/portfolio/square-logo.png'
